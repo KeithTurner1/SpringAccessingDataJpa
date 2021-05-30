c79249cd16c1f9ede09e1cb1527bb2aba879575b
Keith <turnerkj_git@protonmail.com>
2021-05-30 09:03:48 -0400

    Initial commit

---
+++ src/test/java/com/keithturner1/springaccessingdatajpa/SpringAccessingDataJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.keithturner1.springaccessingdatajpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAccessingDataJpaApplicationTests {
+
+   @Test
+   void contextLoads() {
+   }
+
+}
